7dfcf90235b3bc4a9ac5f79291e5f858a27ea6e1
Alex <alex.maksuta@gmail.com>
2024-09-16 10:13:08 +0300

    Variable base

--- main.cpp
+++ main.cpp
@@ -4,44 +4,45 @@
 #include <memory>
 #include <cstring>
 #include <exception>
+template<std::size_t N = 4>
 class BigNum
 {
+	using number = std::uint16_t;
 public:
-	BigNum(const std::string& str): BigNum(new std::uint16_t[str.size()/4 + 2]{}, str.size()/4 + 2)
+	BigNum(const std::string& str): BigNum(new number[str.size()/N + 2]{}, str.size()/N + 2)
 	{
-		std::uint16_t size = 0;
-		std::uint16_t* buf = this->pointerToArr_();
+		number* buf = this->pointerToArr_();
 		std::size_t position = 0;
 		for(auto i = str.rbegin(); i != str.rend(); i++, position++)
 		{
-			std::size_t index = position/4;
-			std::size_t num = position%4;
+			std::size_t index = position/N;
+			std::size_t num = position%N;
 			if(!('0' <= (*i) && *i <= '9'))
 				throw std::runtime_error("Not in base10");
 			
-			std::uint16_t result = *i - '0'; 
-			for(std::uint8_t i = 0; i < num; i++)
+			number result = *i - '0'; 
+			for(std::size_t i = 0; i < num; i++)
 			{
 				result*=10;
 			}
 			*(buf+index) += result;
 		}
-		std::uint16_t index = str.size()/4;
-		if(str.size()%4 != 0)
+		std::size_t index = str.size()/N;
+		if(str.size()%N != 0)
 		{
 			index++;
 		}
-		this->counter_() = index;	
+		this->counter_() = index;
 	}
 
 
-	BigNum(std::size_t capacity = 16) : BigNum(new std::uint16_t[capacity]{}, capacity)
+	BigNum(std::size_t capacity = 16) : BigNum(new number[capacity], capacity)
 	{
 		this->counter_() = 1;
 		(*this->pointerToArr_()) = 0;	
 	}
 
-	BigNum(const BigNum& other) : BigNum(new std::uint16_t[other.capacity_], other.capacity_)
+	BigNum(const BigNum& other) : BigNum(new number[other.capacity_], other.capacity_)
 	{
 		std::memcpy(this->arr_, other.arr_, other.capacity_*sizeof(std::uint16_t)); //copy
 	}
@@ -50,7 +51,7 @@ public:
 	{
 		this->~BigNum();//destroy old
 
-		std::uint16_t* newArr = new std::uint16_t[other.capacity_]; //allocate memomy
+		number* newArr = new number[other.capacity_]; //allocate memomy
 		std::memcpy(newArr, other.arr_, other.capacity_ * sizeof(std::uint16_t)); //copy
 		this->arr_ = newArr; //set new arr
 		return *this;
@@ -65,48 +66,104 @@ public:
 
 	BigNum& operator-=(const BigNum& other)
 	{
-		const std::uint16_t count = other.size(); 
-	 	std::uint16_t* thisBuf = this->pointerToArr_();
-	       	const std::uint16_t* otherBuf = other.pointerToArr_();	
+		const number count = other.size();
+		const number thisCount = this->size(); 
+		if(count > thisCount)
+			throw std::runtime_error("This object is less than other");
+
+
+	 	number* thisBuf = this->pointerToArr_();
+	       	const number* otherBuf = other.pointerToArr_();	
 		bool sign = false;
-		for(std::uint16_t i = 0; i < count; i++)
+		for(number i = 0; i < count; i++)
 		{
-			std::uint16_t thisValue = *(thisBuf+i);
-			const std::uint16_t otherValue = *(otherBuf+i);
+			number thisValue = *(thisBuf+i);
+			const number otherValue = *(otherBuf+i);
 			thisValue -= (sign)?(1):(0);
 			sign = thisValue < otherValue;
 
 			if(sign)
-				thisValue = BigNum::O_ + thisValue - otherValue;
+				thisValue = BigNum::pow(N) + thisValue - otherValue;
 			else
 				thisValue = thisValue - otherValue;
 
 			*(thisBuf + i) = thisValue;
 		}
+		if(sign && (count == thisCount))
+			throw std::runtime_error("This object is less than other");
+
 		*(thisBuf+count)-=(sign)?(1):(0);
 
-		for(std::uint16_t i = this->size()-1; i > 0 && (*(thisBuf+i) == 0); i--)
+		for(number i = this->size()-1; i > 0 && (*(thisBuf+i) == 0); i--)
 		{
 			this->counter_()--;
 		}
 		return *this;
 	}
 
-	bool operator >(const BigNum& other)
+	BigNum operator+(const BigNum& other) const
 	{
-		std::uint16_t thisSize = this->size();
-		std::uint16_t otherSize = other.size();
+		BigNum newNum (*this);
+		newNum+=other;
+		return newNum;
+	}
+
+	BigNum& operator+=(const BigNum& other)
+	{
+		number thisSize = this->size();
+                number otherSize = other.size();
+		number thisCapacity = this->capacity_;
+		number* thisBuf = this->pointerToArr_();
+		const number* otherBuf = other.pointerToArr_();
+		if(otherSize >= thisCapacity)
+		{
+			this->reallocate(otherSize+1);
+			thisBuf = this->pointerToArr_();
+		}
+		for(;thisSize < otherSize; thisSize++)
+		{
+			*(thisBuf + thisSize) = 0;
+		}
+		bool sign = false;
+		for(number i = 0; i < thisSize; i++)
+		{
+			number thisValue = *(thisBuf+i);
+			number otherValue = *(otherBuf+i);
+			thisValue+=otherValue;
+			thisValue+=(sign)?(1):(0);
+			sign = thisValue >= BigNum::pow(N);
+			if(sign)
+			{
+				thisValue -= BigNum::pow(N);
+			}
+			*(thisBuf+i) = thisValue;
+		}
+
+		if(sign)
+		{
+			*(thisBuf+thisSize) = 1;
+			thisSize++;
+		}
+		this->counter_() = thisSize;
+		return *this;
+		
+	}
+
+	bool operator >(const BigNum& other) const noexcept
+	{
+		number thisSize = this->size();
+		number otherSize = other.size();
 		if(thisSize > otherSize)
 			return true;
 		else if(thisSize < otherSize)
 			return false;
 
-		std::uint16_t* thisBuf = this->pointerToArr_();
-		std::uint16_t* otherBuf = other.pointerToArr_();
-		for(std::uint16_t i = 0; i < thisSize; i++)
+		number* thisBuf = this->pointerToArr_();
+		number* otherBuf = other.pointerToArr_();
+		for(number i = 0; i < thisSize; i++)
 		{
-			std::uint16_t thisValue = *(thisBuf + (thisSize - 1) - i);
-			std::uint16_t otherValue = *(otherBuf + (otherSize - 1) -i);
+			number thisValue = *(thisBuf + (thisSize - 1) - i);
+			number otherValue = *(otherBuf + (otherSize - 1) -i);
 			if(thisValue > otherValue)
 				return true;
 
@@ -115,6 +172,31 @@ public:
 		}
 		return false;
 	}
+	bool operator < (const BigNum& other) const noexcept
+	{
+		return other > (*this);
+	}
+
+	bool operator !=(const BigNum& other) const noexcept
+	{
+		return ((*this) > other) && ((*this) < other);
+	}
+
+	bool operator ==(const BigNum& other) const noexcept
+	{
+		return !((*this) != other);
+	}
+
+	bool operator >=(const BigNum& other) const noexcept
+	{
+		return (*this > other) || (*this == other);
+	}
+	bool operator <=(const BigNum& other) const noexcept
+	{
+		return (*this < other) || (*this == other); 
+	}
+	
+
 
 	void debug()
 	{
@@ -126,77 +208,88 @@ public:
 
 	~BigNum()
 	{
-		std::memset(arr_, 0, this->capacity_ * sizeof(std::uint16_t)); //clear
+		std::memset(arr_, 0, this->capacity_ * sizeof(number)); //clear
 		this->capacity_ = 0;
-		delete[] this->arr_;
+		delete[] std::launder(this->arr_);
 	}
 
-	std::uint16_t size() const noexcept
+	number size() const noexcept
 	{
 		return *(this->arr_);
 	}
 
 	operator std::string()
 	{
-		std::string result(this->size()*4, '0');
-		std::uint16_t size = this->size();
-		std::uint16_t* buf = this->pointerToArr_();
-		for(std::uint16_t i = 0; i < size; i++)
+		std::string result(this->size()*N, '0');
+		number size = this->size();
+		number* buf = this->pointerToArr_();
+		for(number i = 0; i < size; i++)
 		{
-			std::uint16_t num = *(buf + size - 1 - i); 
-			std::size_t O = BigNum::O_/10; 
-			for(int q = 3; q >= 0 ; q-- )
+			number num = *(buf + size - 1 - i); 
+			number O = BigNum::pow(N - 1); 
+			for(int q = N-1; q >= 0 ; q-- )
 			{
-				std::uint16_t c = (num/O);
+				number c = (num/O);
 				num-=c*O;
 				O/=10;
-				result.at(i*4 + 3 - q) = c + '0';
+				result.at(i*N + N-1 - q) = c + '0';
 			}	
 			
 		}
 		return result;
 	}
-
 private:
-	BigNum(std::uint16_t* arr, std::size_t capacity): arr_(arr), capacity_(capacity)
+	BigNum(number* arr, std::size_t capacity): arr_(arr), capacity_(capacity)
 	{}
 	
 
-	std::uint16_t& counter_() const noexcept
+	number& counter_() const noexcept
 	{
 		return *(this->arr_);
 	}
 
-	std::uint16_t* pointerToArr_() const noexcept
+	number* pointerToArr_() const noexcept
 	{
 		return this->arr_ + 1;
 	}
 
-	void reallocate(std::size_t add = 10)
+	void reallocate(std::size_t size)
 	{
-		std::size_t newCapacity = this->capacity_ + add;
-		std::uint16_t* newArr = new uint16_t[this->capacity_ + add];
-		std::memcpy(newArr, this->arr_, this->capacity_ * sizeof(std::uint16_t));
-		delete[] this->arr_;
+		number* newArr = new number[size+1]{}; 
+		number* old = this->arr_;
+		std::memcpy(newArr, old, this->capacity_ * sizeof(number));
+		delete[] old;
 		this->arr_ = newArr;
-		this->capacity_ = newCapacity;
+		this->capacity_ = size+1;
 	}
-
+	
+	static constexpr number pow(const number n)
+	{
+		number result = 1;
+		for(std::size_t i = 0; i < n; i++ )
+		{
+			result *= 10;
+		}
+		return result;
+	}
 	
 private:
-	std::uint16_t* arr_;
+	number* arr_;
 	std::size_t capacity_;
-	static const std::uint16_t O_ = 10000;
 };
 
-
+ 
 
 int main()
 {
-	BigNum A("8278473877837858937895789375897389578937589738975893758973895789375897389587362964785278582486264879276948727854");
-	BigNum B("3410151683789593856926894729872875482798168735197489628548248972646275872649728467826498728642649828472987489628");
-	if(A > B){
-		BigNum C = A-B;
-		std::cout << static_cast<std::string> (C) << std::endl;
-	}
+	BigNum A("99994242423131331394444424242424244");
+	BigNum B("14242424244242433424262476264672864");
+	BigNum C = B - A;
+	A.debug();
+	std::cout << std::endl;
+	B.debug();
+	std::cout << std::endl;
+	C.debug();
+	std::cout << std::endl;
+	std::cout << (A != A) << std::endl;
 }
